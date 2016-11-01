16574e13746a0f53480713317372c7ca23cc7399
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2016-11-01 17:00:57 +0100

    CountWords() added

--- LiczenieWPLiku/LiczenieWPLiku/Program.cs
+++ LiczenieWPLiku/LiczenieWPLiku/Program.cs
@@ -10,6 +10,49 @@ namespace LiczenieWPLiku
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("===================\nMenu:\n1. Oblicz liczbę wierszy\n2. Oblicz liczbę wyrazów");
+                string odp = Console.ReadLine();
+                switch (odp)
+                {
+                    case "1":
+                        CountRows();
+                        break;
+                    case "2":
+                        CountWords();
+                        break;
+                }
+            }
+        }
+
+        public static void CountRows()
+        {
+            ;
+        }
+
+        public static void CountWords()
+        {
+            Console.WriteLine("Podaj ścieżkę do pliku lub pozostaw puste (przykładowy plik tekstowy) i wciśnij Enter.");
+            string odp = "";
+            string text = "";
+
+            odp += Console.ReadLine();
+            if (odp.Equals(""))
+                text = System.IO.File.ReadAllText(@"piosenka.txt");
+            else
+            {
+                try
+                {
+                    text = System.IO.File.ReadAllText(odp);
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            string[] words = text.Split(new char [] { '\t', '\n', ' '});
+            Console.WriteLine("Słów w tekście jest " +words.Length +".");
         }
     }
 }
