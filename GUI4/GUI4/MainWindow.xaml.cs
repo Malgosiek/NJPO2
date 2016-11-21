4761ad036b344ea8b427430386bbe2f68d207c9d
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2016-11-21 19:19:52 +0100

    Added: checking PESEL

---
+++ GUI4/GUI4/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string pesel = textBox.Text;
+            int[] weight = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int sum = 0;
+            
+            if(pesel.Length == 11)
+            {
+                for (int i = 0; i < 10; i++)
+                    sum += int.Parse(pesel[i].ToString()) * weight[i];
+                sum %= 10;
+                sum = 10 - sum;
+                sum %= 10;
+                if (sum == int.Parse(pesel[10].ToString()))
+                    textBlock2.Text = "PESEL poprawny!";
+                else
+                    textBlock2.Text = "PESEL niepoprawny!";
+            }
+        }
+    }
+}
