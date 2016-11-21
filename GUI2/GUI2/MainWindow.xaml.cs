c24d193da3ea0628f78722d7fd11b7fbb027852d
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2016-11-21 18:35:02 +0100

    Finished program

--- GUI2/GUI2/MainWindow.xaml.cs
+++ GUI2/GUI2/MainWindow.xaml.cs
@@ -20,9 +20,46 @@ namespace GUI2
     /// </summary>
     public partial class MainWindow : Window
     {
+        double tmp1, tmp2;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            tmp1 = double.Parse(textBox1.Text);
+            tmp2 = double.Parse(textBox2.Text);
+
+            if (radioButton1.IsChecked == true)
+                Addition(tmp1, tmp2);
+            if (radioButton2.IsChecked == true)
+                Substraction(tmp1, tmp2);
+            if (radioButton3.IsChecked == true)
+                Multiplication(tmp1, tmp2);
+            if (radioButton4.IsChecked == true)
+                Division(tmp1, tmp2);
+        }
+
+        private void Addition(double tmp1, double tmp2)
+        {
+            textBox.Text = "" + (tmp1 + tmp2).ToString();
+        }
+        private void Substraction(double tmp1, double tmp2)
+        {
+            textBox.Text = "" + (tmp1 - tmp2).ToString();
+        }
+        private void Multiplication(double tmp1, double tmp2)
+        {
+            textBox.Text = "" + (tmp1 * tmp2).ToString();
+        }
+        private void Division(double tmp1, double tmp2)
+        {
+            if(tmp2 != 0)
+                textBox.Text = "" + (tmp1 / tmp2).ToString();
+            else
+                textBox.Text = "You can not divide by 0!";
         }
     }
 }
