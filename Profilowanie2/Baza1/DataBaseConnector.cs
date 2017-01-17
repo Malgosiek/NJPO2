371dc82f04af682bd064cb2506f1b395367b5571
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2017-01-17 20:55:08 +0100

    Finished project

---
+++ Profilowanie2/Baza1/DataBaseConnector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza1
+{
+    public class DataBaseConnector
+    {
+        private static DataBaseConnector _connectorInstance;
+        // Connection String
+        private string _connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NJPO2;Integrated Security=True";
+        // Provider (System.Data.SqlClient for Microsoft SQL Server)
+        private string _provider = "System.Data.SqlClient";
+
+        private DbProviderFactory _factory;
+
+        private DataBaseConnector()
+        {
+            _factory = DbProviderFactories.GetFactory(_provider);
+        }
+
+        public static DataBaseConnector Instance
+        {
+            get { return _connectorInstance ?? new DataBaseConnector(); }
+        }
+
+        public DbDataReader Query(string query)
+        {
+            // Powoduje zamknięcie zasobu
+            using (DbConnection connection = _factory.CreateConnection())
+            {
+                connection.ConnectionString = _connection;
+                connection.Open();
+
+                DbCommand command = connection.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = query;
+
+                return command.ExecuteReader(); 
+            }
+        }
+
+        public int NonQuery(string query)
+        {
+            // Powoduje zamknięcie zasobów
+            using (DbConnection connection = _factory.CreateConnection())
+            {
+                connection.ConnectionString = _connection;
+                connection.Open();
+
+                DbCommand command = connection.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = query;
+                return command.ExecuteNonQuery();
+            }
+        }
+    }
+}
