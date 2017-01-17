fe4039803b329e086419739ccb55c0f855e0d559
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2017-01-17 20:46:23 +0100

    Finished project

---
+++ Testy3/Baza1/DataBaseConnector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza1
+{
+    public class DataBaseConnector
+    {
+        private static DataBaseConnector _connectorInstance;
+        // Connection String
+        private string _connection = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NJPO2;Integrated Security=True";
+        // Provider (System.Data.SqlClient for Microsoft SQL Server)
+        private string _provider = "System.Data.SqlClient";
+
+        private DbProviderFactory _factory;
+
+        private DataBaseConnector()
+        {
+            _factory = DbProviderFactories.GetFactory(_provider);
+        }
+
+        public static DataBaseConnector Instance
+        {
+            get { return _connectorInstance ?? new DataBaseConnector(); }
+        }
+
+        public DbDataReader Query(string query)
+        {
+            
+            DbConnection connection = _factory.CreateConnection();
+            connection.ConnectionString = _connection;
+            connection.Open();
+
+            DbCommand command = connection.CreateCommand();
+            command.Connection = connection;
+            command.CommandText = query;
+
+            return command.ExecuteReader();
+        }
+
+        public int NonQuery(string query)
+        {
+            using (DbConnection connection = _factory.CreateConnection())
+            {
+                connection.ConnectionString = _connection;
+                connection.Open();
+
+                DbCommand command = connection.CreateCommand();
+                command.Connection = connection;
+                command.CommandText = query;
+                return command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ Testy3/Test/DataBaseTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Baza1;
+using System.Data.Common;
+
+namespace Test
+{
+    [TestClass]
+    public class DataBaseTest
+    {
+        [TestMethod]
+        public void ZwrocInstancje()
+        {
+            // Given
+            DataBaseConnector connector;
+
+            // When
+            connector = DataBaseConnector.Instance;
+
+            // Then
+            Assert.IsNotNull(connector);
+            Assert.IsInstanceOfType(connector, typeof(DataBaseConnector));
+        }
+
+        [TestMethod]
+        public void ZwrocZapytanie()
+        {
+            // Given
+            DataBaseConnector connector = DataBaseConnector.Instance;
+
+            // When
+            DbDataReader reader = connector.Query("SELECT * FROM Contacts");
+            reader.Close();
+
+            // Then
+            Assert.IsNotNull(reader);
+            Assert.IsInstanceOfType(reader, typeof(DbDataReader));
+            Assert.IsTrue(reader.IsClosed);
+        }
+
+        [TestMethod]
+        public void DodajDoBazy()
+        {
+            // Given
+            DataBaseConnector connector = DataBaseConnector.Instance;
+
+            // When
+            int result = connector.NonQuery("INSERT INTO Contacts VALUES (4, 'Jan', 'Nowak', 123456789)");
+
+            // Then
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void NiePowinienDodacDoBazy()
+        {
+            // Given
+            DataBaseConnector connector = DataBaseConnector.Instance;
+            Exception ex = null;
+
+            // When
+            try
+            {
+                int result = connector.NonQuery("INSERT INTO Contacts VALUES (4, 'Jan', 'Nowak', 123456789)");
+            }
+            catch (Exception e)
+            {
+                ex = e;
+            }
+
+            // Then
+            Assert.IsNotNull(ex);
+            Assert.IsInstanceOfType(ex, typeof(System.Data.SqlClient.SqlException));
+        }
+    }
+}
