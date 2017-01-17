2f1e2c0275f17292960e541966920c0b227fdc8c
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2017-01-17 19:48:23 +0100

    Finished program

---
+++ Testy1/Testy/RownanieKwadratoweTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testy1;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Testy
+{
+    [TestClass]
+    public class RownanieKwadratoweTest
+    {
+        [TestMethod]
+        public void ZwrocDodatniaDelte()
+        {
+            // Given
+            RownanieKwadratowe rownanie = new RownanieKwadratowe(1, 10, 2);
+
+            // When
+            double delta = rownanie.Delta();
+
+            // Then
+            Assert.IsTrue(delta > 0);
+        }
+
+        [TestMethod]
+        public void ZwrocZerowaDelte()
+        {
+            // Given
+            RownanieKwadratowe rownanie = new RownanieKwadratowe(1, 2, 1);
+
+            // When
+            double delta = rownanie.Delta();
+
+            // Then
+            Assert.AreEqual(0, delta);
+        }
+
+        [TestMethod]
+        public void ZwrocUjemnaDelte()
+        {
+            // Given
+            RownanieKwadratowe rownanie = new RownanieKwadratowe(4, 1, 10);
+
+            // When
+            double delta = rownanie.Delta();
+
+            // Then
+            Assert.IsTrue(delta < 0);
+        }
+
+        [TestMethod]
+        public void ZwrocDwaMiejscaZerowe()
+        {
+            // Given
+            RownanieKwadratowe rownanie = new RownanieKwadratowe(1, 10, 2);
+
+            // When
+            List<double> miejsca = rownanie.MiejscaZerowe();
+
+            // Then
+            Assert.AreEqual(2, miejsca.Count);
+        }
+
+        [TestMethod]
+        public void ZwrocJednoMiejsceZerowe()
+        {
+            // Given
+            RownanieKwadratowe rownanie = new RownanieKwadratowe(1, 2, 1);
+
+            // When
+            List<double> miejsca = rownanie.MiejscaZerowe();
+
+            // Then
+            Assert.AreEqual(1, miejsca.Count);
+        }
+
+        [TestMethod]
+        public void BrakMiejscZerowych()
+        {
+            // Given
+            RownanieKwadratowe rownanie = new RownanieKwadratowe(4, 1, 10);
+
+            // When
+            List<double> miejsca = rownanie.MiejscaZerowe();
+
+            // Then
+            Assert.IsNull(miejsca);
+        }
+    }
+}
---
+++ Testy1/Testy1/RownanieKwadratowe.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testy1
+{
+    public class RownanieKwadratowe
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public RownanieKwadratowe(double a = 0, double b = 0, double c = 0)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double Delta()
+        {
+            return (B * B) - (4 * A * C);
+        }
+
+        public List<double> MiejscaZerowe()
+        {
+            double delta = Delta();
+            List<double> miejscaZerowe = new List<double>();
+
+            if (delta == 0)
+            {
+                double x = (-B) / (2 * A);
+                miejscaZerowe.Add(x);
+
+                return miejscaZerowe;
+            }
+
+            if (delta > 0)
+            {
+                double x1 = (-B - Math.Sqrt(delta)) / (2 * A);
+                double x2 = (-B + Math.Sqrt(delta)) / (2 * A);
+
+                miejscaZerowe.Add(x1);
+                miejscaZerowe.Add(x2);
+
+                return miejscaZerowe;
+            }
+
+            return null;
+        }
+    }
+}
