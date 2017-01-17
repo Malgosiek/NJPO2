634536aa46b158633928c39a7f39ab563a8da8d5
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2017-01-17 20:17:28 +0100

    Finished project

---
+++ Testy2/Test/SortowanieTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Testy2;
+
+namespace Test
+{
+    [TestClass]
+    public class SortowanieTest
+    {
+        [TestMethod]
+        public void SortowanieSzybszeNizMinuta()
+        {
+            // Given
+            DateTime start, end;
+            TimeSpan difference;
+            Sortowanie sortowanie = new Sortowanie();
+
+            // When
+            start = DateTime.Now;
+
+            sortowanie.Sortuj();
+
+            end = DateTime.Now;
+
+            difference = end.Subtract(start);
+
+            // Then
+            Assert.IsTrue(difference.TotalMinutes < 1);
+        }
+
+        [TestMethod]
+        public void SortowanieWolniejszeNiz10Sekund()
+        {
+            // Given
+            DateTime start, end;
+            TimeSpan difference;
+            Sortowanie sortowanie = new Sortowanie();
+
+            // When
+            start = DateTime.Now;
+
+            for (int i = 0; i < 5; i++)
+            {
+                sortowanie.Sortuj();
+            }
+
+            end = DateTime.Now;
+
+            difference = end.Subtract(start);
+
+            // Then
+            Assert.IsTrue(difference.TotalSeconds > 10);
+        }
+
+        [TestMethod]
+        public void PoprawnaIloscDanych()
+        {
+            // Given
+            Sortowanie sortowanie = new Sortowanie();
+
+            // When
+            List<int> posortowane = sortowanie.Sortuj();
+
+            // Then
+            Assert.AreEqual(sortowanie.Numbers.Count, posortowane.Count);
+        }
+    }
+}
---
+++ Testy2/Testy2/Sortowanie.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testy2
+{
+    public class Sortowanie
+    {
+        private List<int> _lista;
+
+        public List<int> Numbers
+        {
+            get { return _lista; }
+        }
+
+        public Sortowanie()
+        {
+            _lista = new List<int>();
+
+            Random r = new Random();
+
+            for (int i = 0; i < 10000000; i++)
+            {
+                _lista.Add(r.Next());
+            }
+        }
+
+        public List<int> Sortuj()
+        {
+            return _lista.OrderBy(x => x).ToList();
+        }
+    }
+}
